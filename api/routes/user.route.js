da4d350987da5aab136bd174c5e4ae93c8cb4519
Otávio Katibe <otavio.katibe@aln.senaicimatec.edu.br>
2025-05-07 02:40:50 +0000

    v2.1.1

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.get('/users', verifyToken, getUserProfile);
+router.get('/users', getUserProfile);
 router.get('/secure', verifyToken, secureController.Endpoint);
 
 export default router;
