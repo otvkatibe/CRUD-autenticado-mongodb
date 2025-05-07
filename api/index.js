05651cb11072607b0486a2c5add9b593f577c98a
Otávio Katibe <otavio.katibe@aln.senaicimatec.edu.br>
2025-05-07 19:22:10 +0000

    conectando backend ao frontend

--- api/index.js
+++ api/index.js
@@ -8,11 +8,18 @@ dotenv.config();
 db.connect();
 
 const app = express();
+const cors = require("cors");
 app.use(express.json());
 
 app.use("/", userRoute);
 app.use("/users", userRoute);
 app.use("/workouts", workoutRoute);
+
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.get("/", (req, res) => {
     res.send("Backend está no ar!");
